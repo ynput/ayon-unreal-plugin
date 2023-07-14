5a9092e5b42be953dc9ecd54fe673de5c5541deb
Simone Barbieri <simone.barbieri@hey.com>
2023-07-14 14:21:01 +0100

    Rendering implementation

--- UE_5.1/Ayon/Content/Python/rendering.py
+++ UE_5.1/Ayon/Content/Python/rendering.py
@@ -2,15 +2,11 @@ import os
 
 import unreal
 
-from openpype.settings import get_project_settings
-from openpype.pipeline import Anatomy
 from helpers import (
+    get_params,
     get_subsequences,
+    cast_map_to_str_dict
 )
-from pipeline import (
-    parse_container,
-)
-# from openpype.widgets.message_window import Window
 
 queue = None
 executor = None
@@ -34,152 +30,163 @@ def _job_finish_callback(job, success):
     unreal.log("Individual job completed.")
 
 
-def start_rendering():
+def _parse_container(container):
+    """To get data from container, AssetContainer must be loaded."""
+
+    asset = unreal.EditorAssetLibrary.load_asset(container)
+    data = unreal.EditorAssetLibrary.get_metadata_tag_values(asset)
+    data["objectName"] = asset.get_name()
+    return cast_map_to_str_dict(data)
+
+
+def _process_render_instance(instance):
+    ar = unreal.AssetRegistryHelpers.get_asset_registry()
+
+    sequence_path = instance["sequence"]
+    sequence = ar.get_asset_by_object_path(sequence_path).get_asset()
+
+    sequences = [{
+        "sequence": sequence,
+        "output": f"{instance['output']}",
+        "frame_range": (
+            int(float(instance["frameStart"])),
+            int(float(instance["frameEnd"])) + 1)
+    }]
+    render_list = []
+
+    # Get all the sequences to render. If there are subsequences,
+    # add them and their frame ranges to the render list. We also
+    # use the names for the output paths.
+    for seq in sequences:
+        subscenes = get_subsequences(seq.get('sequence'))
+
+        if subscenes:
+            sequences.extend(
+                {
+                    "sequence": sub_seq.get_sequence(),
+                    "output": (
+                        f"{seq.get('output')}/"
+                        f"{sub_seq.get_sequence().get_name()}"
+                    ),
+                    "frame_range": (
+                        sub_seq.get_start_frame(),
+                        sub_seq.get_end_frame(),
+                    ),
+                }
+                for sub_seq in subscenes
+            )
+        elif "_camera" not in seq.get('sequence').get_name():
+            render_list.append(seq)
+
+    return render_list
+
+
+def _create_render_job(render, instance, config, render_dir, render_format):
+    global queue
+
+    job = queue.allocate_new_job(unreal.MoviePipelineExecutorJob)
+    job.sequence = unreal.SoftObjectPath(instance["master_sequence"])
+    job.map = unreal.SoftObjectPath(instance["master_level"])
+    job.author = "Ayon"
+
+    # If we have a saved configuration, copy it to the job.
+    if config:
+        job.get_configuration().copy_from(config)
+
+    # User data could be used to pass data to the job, that can be
+    # read in the job's OnJobFinished callback. We could,
+    # for instance, pass the AyonPublishInstance's path to the job.
+    # job.user_data = ""
+
+    output_dir = render.get('output')
+    shot_name = render.get('sequence').get_name()
+
+    settings = job.get_configuration().find_or_add_setting_by_class(
+        unreal.MoviePipelineOutputSetting)
+    settings.output_resolution = unreal.IntPoint(1920, 1080)
+    settings.custom_start_frame = render.get("frame_range")[0]
+    settings.custom_end_frame = render.get("frame_range")[1]
+    settings.use_custom_playback_range = True
+    settings.file_name_format = f"{shot_name}" + ".{frame_number}"
+    settings.output_directory.path = f"{render_dir}/{output_dir}"
+
+    job.get_configuration().find_or_add_setting_by_class(
+        unreal.MoviePipelineDeferredPassBase)
+
+    if render_format == "png":
+        job.get_configuration().find_or_add_setting_by_class(
+            unreal.MoviePipelineImageSequenceOutput_PNG)
+    elif render_format == "exr":
+        job.get_configuration().find_or_add_setting_by_class(
+            unreal.MoviePipelineImageSequenceOutput_EXR)
+    elif render_format == "jpg":
+        job.get_configuration().find_or_add_setting_by_class(
+            unreal.MoviePipelineImageSequenceOutput_JPG)
+    elif render_format == "bmp":
+        job.get_configuration().find_or_add_setting_by_class(
+            unreal.MoviePipelineImageSequenceOutput_BMP)
+        
+
+def _execute_render_jobs(preroll_frames):
+    global executor
+    executor = unreal.MoviePipelinePIEExecutor()
+
+    settings = unreal.MoviePipelinePIEExecutorSettings()
+    settings.set_editor_property(
+        "initial_delay_frame_count", preroll_frames)
+
+    executor.on_executor_finished_delegate.add_callable_unique(
+        _queue_finish_callback)
+    executor.on_individual_job_finished_delegate.add_callable_unique(
+        _job_finish_callback)  # Only available on PIE Executor
+    executor.execute(queue)
+
+
+def start_rendering(params):
     """
     Start the rendering process.
+
+    Args:
+        params (dict): Dictionary with the following keys:
+            - selection (list): List of selected assets.
     """
-    unreal.log("Starting rendering...")
-
-    # Get selected sequences
-    assets = unreal.EditorUtilityLibrary.get_selected_assets()
-
-    if not assets:
-        Window(
-            parent=None,
-            title="No assets selected",
-            message="No assets selected. Select a render instance.",
-            level="warning")
-        raise RuntimeError(
-            "No assets selected. You need to select a render instance.")
-
-    # instances = pipeline.ls_inst()
-    instances = [
-        a for a in assets
-        if a.get_class().get_name() == "AyonPublishInstance"]
-
-    inst_data = []
-
-    for i in instances:
-        data = parse_container(i.get_path_name())
-        if data["family"] == "render":
-            inst_data.append(data)
-
-    try:
-        project = os.environ.get("AVALON_PROJECT")
-        anatomy = Anatomy(project)
-        root = anatomy.roots['renders']
-    except Exception as e:
-        raise RuntimeError(
-            "Could not find render root in anatomy settings.") from e
-
-    render_dir = f"{root}/{project}"
-
-    # subsystem = unreal.get_editor_subsystem(
-    #     unreal.MoviePipelineQueueSubsystem)
-    # queue = subsystem.get_queue()
-    global queue
-    queue = unreal.MoviePipelineQueue()
+    (selection, render_dir, config_path, render_format,
+     preroll_frames) = get_params(
+        params, "selection", "render_dir", "config_path", "render_format",
+        "preroll_frames")
 
     ar = unreal.AssetRegistryHelpers.get_asset_registry()
 
-    data = get_project_settings(project)
-    config = None
-    config_path = str(data.get("unreal").get("render_config_path"))
+    instances = []
+
+    for asset_path in selection:
+        asset = ar.get_asset_by_object_path(asset_path).get_asset()
+        if asset.get_class().get_name() == "AyonPublishInstance":
+            instances.append(asset)
+
+    render_instances = []
+
+    for instance in instances:
+        data = _parse_container(instance.get_path_name())
+        if data["family"] == "render":
+            render_instances.append(data)
+
+    global queue
+    queue = unreal.MoviePipelineQueue()
+
     if config_path and unreal.EditorAssetLibrary.does_asset_exist(config_path):
         unreal.log("Found saved render configuration")
         config = ar.get_asset_by_object_path(config_path).get_asset()
 
-    for i in inst_data:
-        sequence = ar.get_asset_by_object_path(i["sequence"]).get_asset()
-
-        sequences = [{
-            "sequence": sequence,
-            "output": f"{i['output']}",
-            "frame_range": (
-                int(float(i["frameStart"])),
-                int(float(i["frameEnd"])) + 1)
-        }]
-        render_list = []
-
-        # Get all the sequences to render. If there are subsequences,
-        # add them and their frame ranges to the render list. We also
-        # use the names for the output paths.
-        for seq in sequences:
-            subscenes = get_subsequences(seq.get('sequence'))
-
-            if subscenes:
-                for sub_seq in subscenes:
-                    sequences.append({
-                        "sequence": sub_seq.get_sequence(),
-                        "output": (f"{seq.get('output')}/"
-                                   f"{sub_seq.get_sequence().get_name()}"),
-                        "frame_range": (
-                            sub_seq.get_start_frame(), sub_seq.get_end_frame())
-                    })
-            else:
-                # Avoid rendering camera sequences
-                if "_camera" not in seq.get('sequence').get_name():
-                    render_list.append(seq)
+    for instance in render_instances:
+        render_list = _process_render_instance(
+            instance, config, render_dir, render_format)
 
         # Create the rendering jobs and add them to the queue.
-        for render_setting in render_list:
-            job = queue.allocate_new_job(unreal.MoviePipelineExecutorJob)
-            job.sequence = unreal.SoftObjectPath(i["master_sequence"])
-            job.map = unreal.SoftObjectPath(i["master_level"])
-            job.author = "Ayon"
-
-            # If we have a saved configuration, copy it to the job.
-            if config:
-                job.get_configuration().copy_from(config)
-
-            # User data could be used to pass data to the job, that can be
-            # read in the job's OnJobFinished callback. We could,
-            # for instance, pass the AyonPublishInstance's path to the job.
-            # job.user_data = ""
-
-            output_dir = render_setting.get('output')
-            shot_name = render_setting.get('sequence').get_name()
-
-            settings = job.get_configuration().find_or_add_setting_by_class(
-                unreal.MoviePipelineOutputSetting)
-            settings.output_resolution = unreal.IntPoint(1920, 1080)
-            settings.custom_start_frame = render_setting.get("frame_range")[0]
-            settings.custom_end_frame = render_setting.get("frame_range")[1]
-            settings.use_custom_playback_range = True
-            settings.file_name_format = f"{shot_name}" + ".{frame_number}"
-            settings.output_directory.path = f"{render_dir}/{output_dir}"
-
-            job.get_configuration().find_or_add_setting_by_class(
-                unreal.MoviePipelineDeferredPassBase)
-
-            render_format = data.get("unreal").get("render_format", "png")
-
-            if render_format == "png":
-                job.get_configuration().find_or_add_setting_by_class(
-                    unreal.MoviePipelineImageSequenceOutput_PNG)
-            elif render_format == "exr":
-                job.get_configuration().find_or_add_setting_by_class(
-                    unreal.MoviePipelineImageSequenceOutput_EXR)
-            elif render_format == "jpg":
-                job.get_configuration().find_or_add_setting_by_class(
-                    unreal.MoviePipelineImageSequenceOutput_JPG)
-            elif render_format == "bmp":
-                job.get_configuration().find_or_add_setting_by_class(
-                    unreal.MoviePipelineImageSequenceOutput_BMP)
+        for render in render_list:
+            _create_render_job(
+                render, instance, config, render_dir, render_format)
 
     # If there are jobs in the queue, start the rendering process.
     if queue.get_jobs():
-        global executor
-        executor = unreal.MoviePipelinePIEExecutor()
-
-        preroll_frames = data.get("unreal").get("preroll_frames", 0)
-
-        settings = unreal.MoviePipelinePIEExecutorSettings()
-        settings.set_editor_property(
-            "initial_delay_frame_count", preroll_frames)
-
-        executor.on_executor_finished_delegate.add_callable_unique(
-            _queue_finish_callback)
-        executor.on_individual_job_finished_delegate.add_callable_unique(
-            _job_finish_callback)  # Only available on PIE Executor
-        executor.execute(queue)
+        _execute_render_jobs(preroll_frames)
