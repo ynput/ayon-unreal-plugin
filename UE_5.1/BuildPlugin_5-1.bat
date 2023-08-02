97a75f02fbf53116eebaaf66fd80d7fed774680d
Jakub Trllo <jakub.trllo@gmail.com>
2023-08-02 10:11:36 +0200

    replaced 'AYON_ROOT' with 'AYON_UNREAL_ROOT'

--- UE_5.1/BuildPlugin_5-1.bat
+++ UE_5.1/BuildPlugin_5-1.bat
@@ -1,7 +1,6 @@
 :: Set or replace UNREAL_ENGINE_51 to
 :: the location of your 5.1 installation.
-:: AYON_ROOT (formely OPENPYPE_ROOT) should point
-:: to AYON Desktop (OpenPype) sources.
+:: AYON_UNREAL_ROOT should point to unreal addon root
 
 SET UNREAL_ENGINE_51=%PROGRAMFILES%\Epic Games\UE_5.1
-%UNREAL_ENGINE_51%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=%AYON_ROOT%\openpype\hosts\unreal\integration\UE_5.1\Ayon\Ayon.uplugin" -Package="%~dp0..\build\UE_5.1"
+%UNREAL_ENGINE_51%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=%AYON_UNREAL_ROOT%\integration\UE_5.1\Ayon\Ayon.uplugin" -Package="%~dp0..\build\UE_5.1"
