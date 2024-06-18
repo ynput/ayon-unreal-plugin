761f5fc08a1c302568371c39dca2e09ed1c83fed
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-06-18 17:17:05 +0200

    :fire: remove support for Unreal 4.27

--- UE_4.27/BuildPlugin_4-27.bat
+++
@@ -1,6 +0,0 @@
-:: Set or replace UNREAL_ENGINE_427 to
-:: the location of your 4.27 installation.
-:: AYON_UNREAL_ROOT should point to unreal addon root
-
-SET UNREAL_ENGINE_427=%PROGRAMFILES%\Epic Games\UE_4.27
-%UNREAL_ENGINE_427%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=%AYON_UNREAL_ROOT%\integration\UE_4.27\Ayon\Ayon.uplugin" -Package="%~dp0..\build\UE_4.27\Ayon"
