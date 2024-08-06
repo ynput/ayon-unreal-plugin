9c6167287ea76bb472ac139eb2501a42bd46a3bf
Kayla Man <moonyuet2008@hotmail.com>
2024-08-06 13:55:50 +0800

    unreal raise exception code tweak - ondrej's comment

--- UE_5.4/Ayon/Content/Python/init_unreal.py
+++ UE_5.4/Ayon/Content/Python/init_unreal.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2024 Ynput s.r.o.
 import unreal
-import qtpy
 try:
+    import qtpy
     from qtpy import QtWidgets
-
-except qtpy.QtBindingNotFoundError as exc:
-    message = "PySide 6 is missing, please visit to https://ayon.ynput.io/docs/addon_unreal_admin for more installation info"
+except ImportError as exc:
+    # this is because `QtBingingsNotFoundError` exception is risen
+    # directly from `import qtpy`
+    if exc.__class__.__name__ != "QtBindingsNotFoundError":
+        raise exc
+    message = "PySide 2 is missing, please visit to https://ayon.ynput.io/docs/addon_unreal_admin for more installation info"
     title = "Notification"
     message_type = unreal.AppMsgType.OK
     default_value = unreal.AppReturnType.NO
