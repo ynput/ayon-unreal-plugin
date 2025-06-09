df8c6585c94d530b4ec1d1a279ee4fbf1606a859
Kayla Man <moonyuet2008@hotmail.com>
2025-06-09 23:51:17 +0800

    make sure the path in bat file is correct

--- UE_5.6/BuildPlugin_5-6.bat
+++ UE_5.6/BuildPlugin_5-6.bat
@@ -3,4 +3,4 @@
 :: AYON_UNREAL_ROOT should point to unreal addon root
 
 SET UNREAL_ENGINE_56=%PROGRAMFILES%\Epic Games\UE_5.6
-%UNREAL_ENGINE_55%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=%AYON_UNREAL_ROOT%\integration\UE_5.6\Ayon\Ayon.uplugin -Package="%~dp0..\build\UE_5.6"
+%UNREAL_ENGINE_56%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=%AYON_UNREAL_ROOT%\integration\UE_5.6\Ayon\Ayon.uplugin -Package="%~dp0..\build\UE_5.6"
